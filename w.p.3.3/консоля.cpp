89842a8987703d0ec735be3e87ed631aa9437a88
Kika7536 <kika7536.p@gmail.com>
2024-09-08 18:21:56 +0300

    Добавьте файлы проекта.

---
+++ w.p.3.3/консоля.cpp
@@ -0,0 +1,20 @@
+﻿#include<iostream>
+using namespace std;
+
+
+void main() {
+	setlocale(LC_ALL, "rus");
+
+	int a;
+
+	cout << "Введите число: ";
+	cin >> a;
+	cout << "-----Проверяем-----" << endl;
+
+	if (a % 2 == 0) {
+		cout << "Число " << a << " - четное" << endl;
+	}
+	else {
+		cout << "Число " << a << " - нечетное" << endl;
+	}
+}
